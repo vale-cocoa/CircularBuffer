5c7a1d78c5f56c3f9a61080705a69689e7a319a1
Valeriano Della Longa <valeriano.dellalonga@me.com>
2020-12-12 14:55:41 +0100

    Modifications to inline-help and README.md file.

--- Sources/CircularBuffer/CircularBuffer.swift
+++ Sources/CircularBuffer/CircularBuffer.swift
@@ -28,6 +28,13 @@
 /// That's due to the fact that `Array` has to shift elements successive its first one in order to keep its indexing order intact.
 /// On the other hand, `CircularBuffer` uses a clever *head* and *tail* internal indexes system which makes possible
 /// not to shift elements when removing/inserting at the first and last indexes.
+/// `CircularBuffer` provides functionalities for storing and removing elements which may overwrite
+/// elements previously stored when its storage capacity has been filled-up, allowing it be used effectively as a ring-buffer.
+/// It also provides functionalities for storing and removing new elements with the increase/decrease of the storage capacity.
+/// These latter functionalities might as well be used in conjuction with the *smart capacity policy*,
+/// that is a strategy for increasing/decreasing the capacity of the storage so that the operations
+/// for elements addition/removal won't trigger the resize of the underlaying memory buffer too often,
+/// hence not affecting the overall performance of those operations.
 public final class CircularBuffer<Element> {
     var elements: UnsafeMutablePointer<Element>
     
